820ec9cb5c18aca933b0b2caaf9542c418a78e1c
Boris Nikitashov <ooohrayyy@gmail.com>
2020-12-24 20:53:24 +0300

    feat: Переписать скрипты для кнопок категорий

--- scripts/third-screen.js
+++ scripts/third-screen.js
@@ -1,2 +1,23 @@
 const previousPage = 'second-screen.html';
 const nextPage = 'fourth-screen.html';
+
+// * Логика кнопок выбора подкатегории
+
+const categoryButtons = document.querySelectorAll('.category');
+
+categoryButtons.forEach((button) => {
+  button.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    const buttonsArray = Array.from(categoryButtons);
+    const otherButtons = buttonsArray.filter(button => !(button === evt.target));
+
+    otherButtons.forEach(button => {
+      button.classList.remove('category_active');
+    });
+
+    const buttonValue = button.querySelector('.category__name').textContent;
+    localStorage.subcategory = buttonValue;
+    button.classList.toggle('category_active');
+  });
+});
