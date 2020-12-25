59944f16434c50f884bd2299976b10061c6495f1
Boris Nikitashov <ooohrayyy@gmail.com>
2020-12-25 16:33:49 +0300

    feat: Прописать валидацию выбора подкатегории

--- scripts/third-screen.js
+++ scripts/third-screen.js
@@ -1,7 +1,9 @@
 // * Объявляем переменные
 
-const previousPage = 'second-screen.html'; // Следующая страница
-const nextPage = 'fourth-screen.html'; // Предыдущая страница
+const headerLinks = document.querySelectorAll('.header__link'); // Ссылки в хэдере
+const nextButtons = document.querySelectorAll('.button_type_next'); // Кнопки «Дальше»
+const backButtons = document.querySelectorAll('.button_type_back'); // Кнопки «Назад»
+const socialButtons = document.querySelectorAll('.footer__link'); // Кнопки соцсетей в футере
 
 const categoryButtons = document.querySelectorAll('.category'); // Кнопки подкатегорий
 const subcategories = document.querySelectorAll('.category__name'); // Названия подкатегорий
@@ -10,9 +12,12 @@ const categoryName = localStorage.category; // Выбранная категор
 const imageButton = document.querySelector('.form__photo-button'); // Кнопка выбора изображения
 const imagePreview = document.querySelector('.form__photo-label'); // Адрес превью иллюстрации
 
+const previousPage = 'second-screen.html'; // Следующая страница
+const nextPage = 'fourth-screen.html'; // Предыдущая страница
+
 // * Объявляем функции
 
-function getSubcategories (category) {
+function getSubcategories (category) { // Рендеринг подкатегории в зависимости от выбранной категории
   switch (category) {
     case 'Образование':
       subcategories[0].textContent = 'Школа';
@@ -51,13 +56,17 @@ function getSubcategories (category) {
   }
 }
 
-function getPicture () {
-  const result = prompt('Введите ссылку на изображение');
+function getPicture () { // Получение ссылки на иллюстрацию
+  const result = prompt('Введите ссылку на иллюстрацию');
   return result;
 }
 
-function setImagePreview (src) {
+function setImagePreview (src) { // Добавление превью иллюстрации
   imagePreview.src = src;
+}
+
+function validationAlert () { // Предупреждение валидации
+  alert('Пожалуйста, выберите подкатегорию');
 }
 
 // * Вызываем функции
@@ -66,7 +75,13 @@ getSubcategories(categoryName);
 
 // * Вешаем слушатели событий
 
-categoryButtons.forEach((button) => {
+headerLinks.forEach((link) => { // Алерты на ссылки в хэдере
+  link.addEventListener('click', () => {
+    alert('Эти ссылки пока не работают');
+  });
+});
+
+categoryButtons.forEach((button) => { // Логика кнопок подкатегорий
   button.addEventListener('click', (evt) => {
     evt.preventDefault();
 
@@ -83,11 +98,38 @@ categoryButtons.forEach((button) => {
   });
 });
 
-imageButton.addEventListener('click', (evt) => {
+imageButton.addEventListener('click', (evt) => { // Логика загрузки иллюстрации
   evt.preventDefault();
 
   const imageSource = getPicture();
 
   localStorage.initiative_image = imageSource;
   setImagePreview(imageSource);
+});
+
+nextButtons.forEach((button) => { // Переход на следующую страницу
+  button.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    const activeCategory = document.querySelector('.category_active');
+
+    if (activeCategory) {
+      window.open(nextPage, '_self', false);
+    } else {
+      validationAlert();
+    }
+  });
+});
+
+backButtons.forEach((button) => { // Переход на страницу назад
+  button.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    window.open(previousPage, '_self', false);
+  });
+});
+
+socialButtons.forEach((button) => { // Алерты на кнопках соцсетей в футере
+  button.addEventListener('click', () => {
+    alert('Тут будет переход на соцсети');
+  });
 });
