2b52a595a4b63e10ade4069ad350e072f5d275db
Boris Nikitashov <ooohrayyy@gmail.com>
2020-12-26 15:37:07 +0300

    feat: Чистить localStorage при загрузке главной страницы

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,5 @@ const createButton = document.querySelector('.button_type_create');
 createButton.addEventListener('click', () => {
   window.open('first-screen.html', '_self', false);
 });
+
+localStorage.clear();
